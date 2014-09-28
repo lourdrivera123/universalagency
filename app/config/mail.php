fb34d27cf8925690f7ccabfa3eefbfc10d663eb5
Universal Agency <universalagencynotification@gmail.com>
2014-09-28 20:56:42 +0800

    change the account for mail

--- app/config/mail.php
+++ app/config/mail.php
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => 'lourdrivera123@gmail.com',
+	'username' => 'universalagencynotification@gmail.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => 'zemgust123',
+	'password' => 'universal1234',
 
 	/*
 	|--------------------------------------------------------------------------
