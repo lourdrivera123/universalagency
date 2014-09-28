c778d97f421ce3f8458277af7ac6f9e8d58fdd70
Universal Agency <universalagencynotification@gmail.com>
2014-09-28 19:06:16 +0800

    mail

--- app/config/mail.php
+++ app/config/mail.php
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'smtp.gmail.com',
+	'host' => 'newmexico.pw',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => 'universalagencynotification@gmail.com',
+	'username' => 'smtp@newmexico.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => 'universal123',
+	'password' => 'test1234',
 
 	/*
 	|--------------------------------------------------------------------------
